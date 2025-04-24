d94155472e6df9393565a3f67df5089e79337aa6
Shina Adewumi <shinaenoch003@gmail.com>
2025-04-23 21:43:07 -0400

    .

--- clientSite/fountain_brothers/vite.config.js
+++ clientSite/fountain_brothers/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['fountain-brothers-v2.onrender.com']
+  }
 })
